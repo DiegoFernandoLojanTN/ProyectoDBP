8d321b4fb71881d0383adb41ecf453208a8661e1
LautishB <diego.lojan@unl.edu.ec>
2022-08-05 23:44:16 -0500

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ferna\OneDrive\Escritorio\MATERIAS\DESARROLLO BASADO EN PLATAFORMAS\UNIDAD 3\ProyectoFinal_LS\DjangoPrueba\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
